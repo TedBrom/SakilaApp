5cf53179150c92056a429480e32a016fbd2165d9
Ed <edwardbromilow@live.co.uk>
2022-09-13 09:37:16 +0100

    added cucumber tests

---
+++ src/test/java/CucumberTests/DisplayFilmsStepDefs.java
@@ -0,0 +1,31 @@
+package CucumberTests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DisplayFilmsStepDefs {
+
+    @Given("the application is running")
+    public void the_application_is_running() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("the {string} loads")
+    public void the_loads(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("the api connects")
+    public void the_api_connects() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("display a list of films")
+    public void display_a_list_of_films() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+}
---
+++ src/test/resources/Cucumber/DisplayFilm.feature
@@ -0,0 +1,12 @@
+Feature: Display films on load
+  Scenario Outline: verify content loads
+    Given the application is running
+    When the "<page>" loads
+    And the api connects
+    Then display a list of films
+
+    Examples:
+      |page  |
+      |Home  |
+      |Actor |
+      |Film  |
